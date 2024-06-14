7ce2d318dda7d10d79a3fab3944f8cfc49d3de34
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-14 19:07:42 +0100

    "Added authentication protection to patients routes"

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -63,3 +63,29 @@ exports.login = async (req, res, next) => {
     //   createSendToken(user, 200, res);
   }
 };
+
+exports.protect = async (req, res, next) => {
+  // 1) Getting token and check of it is there
+  let token;
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    // 2) Verification token
+    if (!token)
+      return next(
+        new AppError('You are not logged in! Please log in to get access', 401)
+      );
+
+    // 3) Check if user still exists
+
+    // 4) Check if user changed password after the JWT was issue
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
--- backend/routes/patientsRoutes.js
+++ backend/routes/patientsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const patientsController = require('../controllers/patientsControllers');
+const authController = require('../controllers/authControllers');
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(patientsController.getAllPatients)
-  .post(patientsController.createPatient);
+  .get(authController.protect, patientsController.getAllPatients)
+  .post(authController.protect, patientsController.createPatient);
 
 module.exports = router;
