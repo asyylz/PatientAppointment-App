5747985cdbf1e5c684e338219caa0fff73d23f53
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-19 22:06:23 +0100

    "Added security features to backend app.js, including helmet, mongoSanitize, xss-clean, and hpp; updated package.json and package-lock.json accordingly"

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,58 @@
 const express = require('express');
 const morgan = require('morgan');
-
-const app = express();
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const doctorsRouter = require('./routes/doctorsRoutes');
 const patientsRouter = require('./routes/patientsRoutes');
 const departmentsRouter = require('./routes/departmentsRoutes');
 const usersRouter = require('./routes/usersRoutes');
 const reviewsRouter = require('./routes/reviewsRoutes');
+const globalErrorHandler = require('./controllers/errorControllers');
+const AppError = require('./utils/appError');
 
-// 1-) MIDDLEWARE
-// app.use(morgan('dev'));
-app.use(express.json());
+const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again  in an hour'
+});
+app.use('/api', limiter);
+
+// Boody parser , reading data from  body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      '$ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Test middlewares
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -21,7 +60,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// 2-) ROUTES
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+// ROUTES
 app.use('/api/v1/doctors', doctorsRouter); // mounting router
 app.use('/api/v1/patients', patientsRouter); // mounting router
 app.use('/api/v1/departments', departmentsRouter); // mounting router
