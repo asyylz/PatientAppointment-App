5390ed0206d55167a82c0de17724135d28d56356
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-19 21:58:15 +0100

    "Fixed typo in cookieOptions variable and added password removal for user object"

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -13,15 +13,16 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  const cokiesOptions = {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true
   };
-  if (process.env.NODE_ENV === 'production') cokiesOptions.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
-  res.cokies('jwt', token, cokiesOptions);
+  res.cokies('jwt', token, cookieOptions);
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
