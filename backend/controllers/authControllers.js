1807aeb2757c2bbab5bfeaf6119e385a978fe5a1
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-19 21:49:04 +0100

    "Added cookie options and security middleware to auth controllers, updated package-lock.json and package.json with new dependencies"

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -13,6 +13,16 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cokiesOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cokiesOptions.secure = true;
+
+  res.cokies('jwt', token, cokiesOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
