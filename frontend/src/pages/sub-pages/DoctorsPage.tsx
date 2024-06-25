aff89c7dab121c42f21616e6a1399410f09da586
asyylz <49663592+asyylz@users.noreply.github.com>
2024-06-25 07:04:20 +0100

    "Added useState and useEffect hooks to implement doctor search functionality and filtering"

--- frontend/src/pages/sub-pages/DoctorsPage.tsx
+++ frontend/src/pages/sub-pages/DoctorsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from './../../store/index';
 import { fetchDoctors } from './../../store/doctors-slice';
@@ -14,18 +14,51 @@ const Doctors: React.FC = () => {
     status,
     error,
   } = useSelector((state: RootState) => state.doctors);
-  
-  console.log(doctors);
+
+  const searchWord = useSelector((state: RootState) => state.search);
 
   const handleSelectDoctor = (doctor: Doctor) => {
     dispatch(doctorActions.selectDoctor(doctor));
   };
+
+  const [filteredDoctors, setFilteredDoctors] = useState(doctors);
 
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchDoctors());
     }
   }, [status, dispatch]);
+
+  useEffect(() => {
+    if (searchWord) {
+      const filtered = doctors
+        .filter(
+          (doctor) =>
+            doctor.firstName.toLowerCase().includes(searchWord.toLowerCase()) ||
+            doctor.lastName.toLowerCase().includes(searchWord.toLowerCase())
+        )
+        .sort((a, b) => {
+          const aMainIncludes = a.firstName
+            .toLowerCase()
+            .includes(searchWord.toLowerCase());
+          const bMainIncludes = b.firstName
+            .toLowerCase()
+            .includes(searchWord.toLowerCase());
+
+          if (aMainIncludes && !bMainIncludes) {
+            return -1; // `a` should come before `b`
+          } else if (!aMainIncludes && bMainIncludes) {
+            return 1; // `b` should come before `a`
+          } else {
+            return 0; // keep the order as is
+          }
+        });
+
+      setFilteredDoctors(filtered);
+    } else {
+      setFilteredDoctors(doctors);
+    }
+  }, [searchWord, doctors]);
 
   return (
     <>
@@ -38,7 +71,7 @@ const Doctors: React.FC = () => {
         {status === 'loading' && <p>Loading...</p>}
 
         {status === 'succeeded' &&
-          doctors.map((doctor: Doctor) => (
+          filteredDoctors.map((doctor: Doctor) => (
             <DoctorProfilCard
               key={doctor.id}
               doctor={doctor as Doctor}
