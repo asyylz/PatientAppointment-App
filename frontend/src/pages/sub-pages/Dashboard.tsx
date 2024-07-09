7ab1a0cf1aa167869cac3dbe438ca940e50c5d75
asyylz <49663592+asyylz@users.noreply.github.com>
2024-07-09 13:23:14 +0100

    "Refactored Dashboard module CSS to use CSS variables, updated icon usage in Dashboard component"

--- frontend/src/pages/sub-pages/Dashboard.tsx
+++ frontend/src/pages/sub-pages/Dashboard.tsx
@@ -4,10 +4,9 @@ import { useSelector } from 'react-redux';
 import GlobalLink from '../../components/UI/GlobalLink';
 import useAxios from '../../hooks/useAxios';
 import AppointmentCard from '../../components/UI/AppointmentCard';
-
+import { FaStethoscope } from 'react-icons/fa';
 
 const Dashboard: React.FC = () => {
-
   const { userData, image } = useSelector(
     (state: RootState) => state.currentUser
   );
@@ -76,9 +75,9 @@ const Dashboard: React.FC = () => {
           </div>
           <div className={`${classes.box} ${classes.box2}`}>
             <p>Upcoming Visits: {total.upcomingAppointments}</p>
-            <svg fill="#CADE70" viewBox="0 0 576 512">
-              <path d="M142.4 21.9c5.6 16.8-3.5 34.9-20.2 40.5L96 71.1V192c0 53 43 96 96 96s96-43 96-96V71.1l-26.1-8.7c-16.8-5.6-25.8-23.7-20.2-40.5s23.7-25.8 40.5-20.2l26.1 8.7C334.4 19.1 352 43.5 352 71.1V192c0 77.2-54.6 141.6-127.3 156.7C231 404.6 278.4 448 336 448c61.9 0 112-50.1 112-112V265.3c-28.3-12.3-48-40.5-48-73.3c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V336c0 97.2-78.8 176-176 176c-92.9 0-168.9-71.9-175.5-163.1C87.2 334.2 32 269.6 32 192V71.1c0-27.5 17.6-52 43.8-60.7l26.1-8.7c16.8-5.6 34.9 3.5 40.5 20.2zM480 224a32 32 0 1 0 0-64 32 32 0 1 0 0 64z" />
-            </svg>
+            <div className={`${classes.icons} ${classes.stethoscope}`}>
+              <FaStethoscope />
+            </div>
           </div>
           <div className={`${classes.box} ${classes.box3}`}>
             <p>Total Doctors : {totalDoctors.length}</p>
@@ -100,34 +99,6 @@ const Dashboard: React.FC = () => {
             <div className={classes.wrapper2}>
               {appointments?.map((appointment: Appointment) => (
                 <AppointmentCard appointment={appointment} />
-                // <div
-                //   className={
-                //     new Date(appointment.appointmentDate) <=
-                //     new Date(new Date().setHours(0, 0, 0, 0))
-                //       ? `${classes.appointmentBox} ${classes.close}`
-                //       : `${classes.appointmentBox} ${classes.open}`
-                //   }
-                // >
-                //   <p>{`Dr. ${appointment?.doctorId?.firstName} ${appointment?.doctorId?.lastName}`}</p>
-                //   <p>
-                //     Department:
-                //     {departments.map((department) => {
-                //       if (
-                //         department._id.toString() ===
-                //         appointment.doctorId.departmentId.toString()
-                //       ) {
-                //         return <span> {department.departmentMain}</span>;
-                //       }
-                //       return null; // Ensure a return value is always provided
-                //     })}
-                //   </p>
-                //   <p>Time: {appointment.time}</p>
-                //   <p>Date: {formatDateForUI(appointment.appointmentDate)}</p>
-                //   <p>Concerns: {appointment.reason}</p>
-                //   <div className={`${classes.icons} ${classes.cross}`}>
-                //     <RxCross2 />
-                //   </div>
-                // </div>
               ))}
             </div>
           </div>
